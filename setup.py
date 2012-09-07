a52c1f8f1b21a8faad459d31ccd2138e8298aa83
Andras Wacha <awacha@gmail.com>
2012-09-07 16:54:12 +0200

    Version 0.0.3
    depends on sastool >= 0.1.3
    
    Signed-off-by: Andras Wacha <awacha@gmail.com>

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 
-setup(name='sasgui', version='0.0.2', author='Andras Wacha',
+setup(name='sasgui', version='0.0.3', author='Andras Wacha',
       author_email='awacha@gmail.com', url='http://github.com/awacha/sasgui',
       description='Graphical User Interface utilities for small-angle scattering',
       packages=find_packages(),
-      install_requires=['sastool>0.0.10'],
+      install_requires=['sastool>=0.1.3'],
       entry_points={'gui_scripts':['sas2dutil = sasgui.sasimagegui:sasimagegui_main'],
                     },
       keywords="saxs sans sas small-angle scattering x-ray neutron gui graphical user interface",
